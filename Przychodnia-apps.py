636d886c67fee9dc35affaec8c64e8aa448311cf
dominikgros454-creator <dominikgros454@gmail.com>
2025-11-05 08:43:54 +0100

    Update Przychodnia-apps.py

--- Przychodnia-apps.py
+++ Przychodnia-apps.py
@@ -3,7 +3,6 @@ import sqlite3
 import struct
 import pandas as pd
 from datetime import datetime, timedelta
-from twilio.rest import Client
 import pytz
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
